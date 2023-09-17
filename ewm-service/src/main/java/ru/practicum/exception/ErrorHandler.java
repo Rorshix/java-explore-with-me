242182595eccffb371b5746efd3ec849cb1b1a79
Ivan <v.chester@yandex.ru>
2023-09-17 21:08:49 +0300

    тест

--- ewm-service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ ewm-service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -24,24 +24,32 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleThrowable(final ValidationException e) {
-        return new ErrorResponse(e.getMessage());
+        String error = String.format("validation exception: %$", e.getMessage());
+        log.info(error);
+        return new ErrorResponse(error);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleThrowable(final MethodArgumentNotValidException e) {
-        return new ErrorResponse(e.getMessage());
+        String error = String.format("method argument exception: %$", e.getMessage());
+        log.info(error);
+        return new ErrorResponse(error);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleEmailExistException(final ConstraintViolationException e) {
-        return new ErrorResponse(e.getMessage());
+        String error = String.format("constraint exception: %$", e.getMessage());
+        log.info(error);
+        return new ErrorResponse(error);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleEmailExistException(final ConflictException e) {
-        return new ErrorResponse(e.getMessage());
+        String error = String.format("conflict exception: %$", e.getMessage());
+        log.info(error);
+        return new ErrorResponse(error);
     }
 }
