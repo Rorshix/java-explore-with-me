72d4034ea9795ff79d8291263753541e5383c460
Ivan <v.chester@yandex.ru>
2023-09-17 22:19:41 +0300

    тест

--- ewm-service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ ewm-service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.practicum.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,14 +8,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleNotFoundException(final NotFoundException e) {
-        return new ErrorResponse(e.getMessage());
+    public Map<String,String> handleNotFoundException(final NotFoundException e) {
+        log.error("Код ошибки:{404},{404}",HttpStatus.NOT_FOUND,e.getMessage());
+        return Map.of("status","NOT_FOUND",
+                "reason","NOT FOUND",
+                "message",e.getMessage());
     }
 
     @ExceptionHandler
